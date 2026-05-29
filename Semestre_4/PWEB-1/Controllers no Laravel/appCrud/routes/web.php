52c53e1dbaddac508925617d951326b81078c21c
Luidy Costa <lluidycosta2024@gmail.com>
2026-05-29 20:30:45 -0300

    feat: adiciona rota GET apontando para index de cursos

--- Semestre_4/PWEB-1/Controllers no Laravel/appCrud/routes/web.php
+++ Semestre_4/PWEB-1/Controllers no Laravel/appCrud/routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CursoController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/cursos', [CursoController::class, 'index'])->name('cursos');
