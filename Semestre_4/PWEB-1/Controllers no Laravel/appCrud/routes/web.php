608c3b8718df53f5636242e0a7513fcad071251e
Luidy Costa <lluidycosta2024@gmail.com>
2026-05-29 20:43:25 -0300

    feat: adiciona rota GET para listagem de cursos

--- Semestre_4/PWEB-1/Controllers no Laravel/appCrud/routes/web.php
+++ Semestre_4/PWEB-1/Controllers no Laravel/appCrud/routes/web.php
@@ -6,3 +6,5 @@ use App\Http\Controllers\CursoController;
 Route::get('/cursos', [CursoController::class, 'index'])->name('cursos');
 
 Route::get('/cursos/novo', [CursoController::class, 'create'])->name('cursos.create');
+
+Route::get('/cursos/lista', [CursoController::class, 'listagem'])->name('cursos.listagem');
