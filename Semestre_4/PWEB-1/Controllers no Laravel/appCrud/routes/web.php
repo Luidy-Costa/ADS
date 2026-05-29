8034bb7bda2e0f7e321c973a596504fed04a05da
Luidy Costa <lluidycosta2024@gmail.com>
2026-05-29 20:37:18 -0300

    feat: adiciona rota GET para o formulario de novo curso

--- Semestre_4/PWEB-1/Controllers no Laravel/appCrud/routes/web.php
+++ Semestre_4/PWEB-1/Controllers no Laravel/appCrud/routes/web.php
@@ -4,3 +4,5 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\CursoController;
 
 Route::get('/cursos', [CursoController::class, 'index'])->name('cursos');
+
+Route::get('/cursos/novo', [CursoController::class, 'create'])->name('cursos.create');
