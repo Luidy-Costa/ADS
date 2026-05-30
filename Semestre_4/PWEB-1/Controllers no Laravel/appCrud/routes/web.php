10f70bfae9bd96e8138d282956f00f21911f14c5
Luidy Costa <lluidycosta2024@gmail.com>
2026-05-29 21:12:04 -0300

    feat: adiciona rota GET com parametro dinamico id

--- Semestre_4/PWEB-1/Controllers no Laravel/appCrud/routes/web.php
+++ Semestre_4/PWEB-1/Controllers no Laravel/appCrud/routes/web.php
@@ -8,3 +8,5 @@ Route::get('/cursos', [CursoController::class, 'index'])->name('cursos');
 Route::get('/cursos/novo', [CursoController::class, 'create'])->name('cursos.create');
 
 Route::get('/cursos/lista', [CursoController::class, 'listagem'])->name('cursos.listagem');
+
+Route::get('/cursos/{id}', [CursoController::class, 'show'])->name('cursos.show');
