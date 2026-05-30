c9910ec17812a8f7a13379d45b0ac8622620d042
Luidy Costa <lluidycosta2024@gmail.com>
2026-05-29 21:13:54 -0300

    feat: adiciona formulario de cadastro na view create

--- Semestre_4/PWEB-1/Controllers no Laravel/appCrud/resources/views/cursos/create.blade.php
+++ Semestre_4/PWEB-1/Controllers no Laravel/appCrud/resources/views/cursos/create.blade.php
@@ -6,5 +6,12 @@
 </head>
 <body>
     <h1>Cadastro de Curso</h1>
+    
+    <form action="/cursos" method="POST">
+        @csrf
+        <label>Nome do curso:</label><br>
+        <input type="text" name="nome"><br><br>
+        <button type="submit">Cadastrar</button>
+    </form>
 </body>
 </html>
